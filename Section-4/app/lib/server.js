980ed78cbe3a1db00f4257fbe820169b257b8e45
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-10-15 15:34:52 +0545

    Refactoring for a GUI

---
+++ Section-4/app/lib/server.js
@@ -0,0 +1,146 @@
+/**
+ * Server-related tasks
+ * 
+ */
+
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const path = require('path');
+const StringDecoder = require('string_decoder').StringDecoder;
+const url = require('url');
+const util = require('util');
+
+const config = require('./config');
+const handlers = require('./handlers');
+const helpers = require('./helpers');
+
+const debug = util.debuglog('server');
+
+// Instantiate the server module object
+const server = {};
+
+// Instantiate the HTTP server
+server.httpServer = http.createServer(function (req, res) {
+  server.unifiedServer(req, res);
+});
+
+// Instantiate the HTTPS server
+server.httpsServerOptions = {
+  key: fs.readFileSync(path.join(__dirname, '/../https/key.pem')),
+  cert: fs.readFileSync(path.join(__dirname, '/../https/cert.pem'))
+};
+server.httpsServer = https.createServer(server.httpsServerOptions, function (req, res) {
+  server.unifiedServer(req, res);
+});
+
+// All the server logic for both the http and https server
+server.unifiedServer = function (req, res) {
+
+  // Parse the url
+  const parsedUrl = url.parse(req.url, true);
+
+  // Get the path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+
+  // Get the query string as an object
+  const queryStringObject = parsedUrl.query;
+
+  // Get the HTTP method
+  const method = req.method.toLowerCase();
+
+  // Get the headers as an object
+  const headers = req.headers;
+
+  // Get the payload, if any
+  const decoder = new StringDecoder('utf-8');
+  let buffer = '';
+  req.on('data', function (data) {
+    buffer += decoder.write(data);
+  });
+  req.on('end', function () {
+    buffer += decoder.end();
+
+    // Check the router for a matching path for a handler.
+    // If one is not found, use the notFound handler instead.
+    const chosenHandler = typeof (server.router[trimmedPath]) !== 'undefined' ?
+      server.router[trimmedPath] : handlers.notFound;
+
+    // Construct the data object to send to the handler
+    const data = {
+      trimmedPath,
+      queryStringObject,
+      method,
+      headers,
+      payload: helpers.parseJsonToObject(buffer)
+    };
+
+    // Route the request to the handler specified in the router
+    chosenHandler(data, function (statusCode, payload, contentType) {
+
+      // Determine the type of response (fallback to JSON)
+      contentType = typeof (contentType) == 'string' ? contentType : 'json';
+
+      // Use the status code returned from the handler, or set the default status code to 200
+      statusCode = typeof (statusCode) == 'number' ? statusCode : 200;
+
+      // Return the response parts that are content-type specific
+      let payloadString = '';
+      if (contentType == 'json') {
+        res.setHeader('Content-Type', 'application/json');
+        payload = typeof (payload) == 'object' ? payload : {};
+        payloadString = JSON.stringify(payload);
+      }
+      if (contentType == 'html') {
+        res.setHeader('Content-Type', 'text/html');
+        payloadString = typeof (payload) == 'string' ? payload : '';
+      }
+
+      // Return the response
+      res.writeHead(statusCode);
+      res.end(payloadString);
+
+      // If the response is 200, print green, otherwise print red
+      if (statusCode == 200) {
+        debug('\x1b[32m%s\x1b[0m', `${method.toUpperCase()} /${trimmedPath} ${statusCode}`);
+      } else {
+        debug('\x1b[31m%s\x1b[0m', `${method.toUpperCase()} /${trimmedPath} ${statusCode}`);
+      }
+    });
+  });
+};
+
+// Define the request router
+server.router = {
+  '': handlers.index,
+  'account/create': handlers.accountCreate,
+  'account/edit': handlers.accountEdit,
+  'account/deleted': handlers.accountDeleted,
+  'session/create': handlers.sessionCreate,
+  'session/deleted': handlers.sessionDeleted,
+  'checks/all': handlers.checksList,
+  'checks/create': handlers.checksCreate,
+  'checks/edit': handlers.checksEdit,
+  ping: handlers.ping,
+  'api/users': handlers.users,
+  'api/tokens': handlers.tokens,
+  'api/checks': handlers.checks
+};
+
+// Init script
+server.init = function () {
+  // Start the HTTP server
+  server.httpServer.listen(config.httpPort, function () {
+    console.log('\x1b[36m%s\x1b[0m', `The server is up and running on port ${config.httpPort}`);
+  });
+
+  // Start the HTPS server
+  server.httpsServer.listen(config.httpsPort, function () {
+    console.log('\x1b[35m%s\x1b[0m', `The server is up and running on port ${config.httpsPort}`);
+  });
+}
+
+// Export the module
+module.exports = server;
