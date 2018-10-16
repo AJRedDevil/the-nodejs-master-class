d66ab016659c09bfb44142f6f16b5d287153c8ff
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2018-10-16 11:43:22 +0545

    Page 2 - Create Account

--- Section-4/app/lib/handlers.js
+++ Section-4/app/lib/handlers.js
@@ -48,6 +48,37 @@ handlers.index = function (data, callback) {
   }
 };
 
+// Create Account
+handlers.accountCreate = function (data, callback) {
+  // Reject any request that isn't a get
+  if (data.method == 'get') {
+    // Prepare data for interpolation
+    const templateData = {
+      'head.title': 'Create an Account',
+      'head.description': 'Signup is easy and only takes a few seconds',
+      'body.class': 'accountCreate'
+    };
+    // Read in a template as a string
+    helpers.getTemplate('accountCreate', templateData, function (err, str) {
+      if (!err && str) {
+        // Add the universal header and footer
+        helpers.addUniversalTemplates(str, templateData, function (err, str) {
+          if (!err && str) {
+            // Return that page as HTML
+            callback(200, str, 'html');
+          } else {
+            callback(500, undeinfed, 'html');
+          }
+        });
+      } else {
+        callback(500, undeinfed, 'html');
+      }
+    });
+  } else {
+    callback(405, undefined, 'html');
+  }
+};
+
 // Favicon
 handlers.favicon = function (data, callback) {
   // Reject any request that isn't a get
